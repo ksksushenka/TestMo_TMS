92d2294b107ded2090dd8551d10a8c3b7cc7ad9f
Kseniya Grebenyuk <kgrebenyuk@caeonline.com>
2023-07-02 11:00:36 -0500

    Added UI tests (positive and negative).

---
+++ TestMo_TMS/Pages/BasePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestMo_TMS.Core;
+using TestMo_TMS.Utilites.Configuration;
+
+namespace TestMo_TMS.Pages
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver Driver;
+        protected WaitService WaitService;
+
+        public BasePage(IWebDriver driver, bool openPageByUrl)
+        {
+            Driver = driver;
+            WaitService = new WaitService(Driver);
+
+            if (openPageByUrl)
+            {
+                OpenPage();
+            }
+        }
+
+        public abstract void OpenPage();
+        public abstract bool IsPageOpened();
+    }
+}
---
+++ TestMo_TMS/Wrappers/Button.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMo_TMS.Wrappers
+{
+    public class Button
+    {
+        private readonly UIElement _uiElement;
+
+        public Button(IWebDriver? driver, By @by)
+        {
+            _uiElement = new UIElement(driver, @by);
+        }
+
+        public void Click() => _uiElement.Click();
+
+        public string Text => _uiElement.Text;
+
+        public bool Displayed => _uiElement.Displayed;
+    }
+}
